8c23e9bf1f2b7d5fbe3cfdf7029384ec05be05ad
Beterraba <ls6182315@gmail.com>
2023-06-28 15:18:41 -0300

    feat: test Ingredient class

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,24 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    arroz = Ingredient("arroz")
+    feijao = Ingredient("feijao")
+    manteiga = Ingredient("manteiga")
+    assert manteiga.restrictions == {
+        Restriction.LACTOSE, Restriction.ANIMAL_DERIVED
+    }
+
+    assert arroz.name == "arroz"
+    assert feijao.name == "feijao"
+    assert manteiga.name == "manteiga"
+
+    assert hash(arroz) == hash("arroz")
+    assert hash(feijao) == hash("feijao")
+    assert hash(manteiga) == hash("manteiga")
+
+    assert repr(arroz) == "Ingredient('arroz')"
+    assert repr(feijao) == "Ingredient('feijao')"
+
+    assert arroz != feijao
